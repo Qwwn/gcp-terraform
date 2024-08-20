4e3785cf2557e14b5b85e13417eda198ceba4e9b
Qwwn <agyl1restu@gmail.com>
2024-08-20 19:43:53 +0700

    initial commit

--- main.tf
+++ main.tf
@@ -20,6 +20,6 @@ resource "google_storage_object_access_control" "public_rule" {
 
 resource "google_storage_bucket_object" "static_site_src" {
   name = "index.html"
-  source = "../website/index.html"
+  source = "./index.html"
   bucket = google_storage_bucket.website.name
 }
