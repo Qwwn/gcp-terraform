978a32fbcdf9f170fa9ab909988c825f4254b811
Qwwn <agyl1restu@gmail.com>
2024-08-20 19:26:33 +0700

    initial commit

--- main.tf
+++ main.tf
@@ -6,3 +6,20 @@ resource "google_storage_bucket" "auto-expire" {
 
   public_access_prevention = "enforced"
 }
+
+# make new object public
+
+resource "google_storage_object_access_control" "public_rule" {
+  object = google_storage_bucket_object.static_site_src.name
+  bucket = google_storage_bucket.website.name
+  role = "READER"
+  entity = "allUsers"
+}
+
+# upload html to bucket
+
+resource "google_storage_bucket_object" "static_site_src" {
+  name = "index.html"
+  source = "../website/index.html"
+  bucket = google_storage_bucket.website.name
+}
